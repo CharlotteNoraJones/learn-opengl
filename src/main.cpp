9cbd2c7cc1e6c37e9342c4bdd621fea0dcc5a075
CharlotteNoraJones <charlottenorajones@gmail.com>
2024-09-08 11:48:03 -0500

    GLAD and GLFW are now working.

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,9 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+const int WIDTH = 640;
+const int HEIGHT = 480;
+
 static void error_callback(int error, const char *description)
 {
     fprintf(stderr, "Error: %s\n", description);
@@ -16,6 +19,11 @@ static void key_callback(GLFWwindow *window, int key, int scancode, int action, 
     }
 }
 
+void framebuffer_size_callback(GLFWwindow *window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
 int main(void)
 {
     glfwSetErrorCallback(error_callback);
@@ -25,12 +33,11 @@ int main(void)
         fprintf(stderr, "Could not initialize GLFW\n");
         return -1;
     }
-
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWwindow *window = glfwCreateWindow(640, 480, "Trongle!", nullptr, nullptr);
+    GLFWwindow *window = glfwCreateWindow(WIDTH, HEIGHT, "Trongle!", nullptr, nullptr);
     if (!window)
     {
         glfwTerminate();
@@ -39,14 +46,30 @@ int main(void)
         return -1;
     }
 
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        fprintf(stderr, "Failed to initialize GLAD\n");
+        glfwDestroyWindow(window);
+        window = nullptr;
+        glfwTerminate();
+        return -1;
+    }
+
+    glViewport(0, 0, 640, 480);
+
     glfwSetKeyCallback(window, key_callback);
 
-    glfwMakeContextCurrent(window);
     glfwSwapInterval(1);
 
     glfwSetWindowShouldClose(window, GLFW_FALSE);
     while (!glfwWindowShouldClose(window))
     {
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
