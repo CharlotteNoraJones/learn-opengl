5ab68a630d6ae0a29b73851f917b4bc1f252b2dd
CharlotteNoraJones <charlottenorajones@gmail.com>
2024-09-08 11:03:52 -0500

    Opened a window and GLFW.

---
+++ src/main.cpp
@@ -0,0 +1,58 @@
+#include <stdio.h>
+
+#include <GLFW/glfw3.h>
+
+static void error_callback(int error, const char *description)
+{
+    fprintf(stderr, "Error: %s\n", description);
+}
+
+static void key_callback(GLFWwindow *window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+    }
+}
+
+int main(void)
+{
+    glfwSetErrorCallback(error_callback);
+
+    if (!glfwInit())
+    {
+        fprintf(stderr, "Could not initialize GLFW\n");
+        return -1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow *window = glfwCreateWindow(640, 480, "Trongle!", nullptr, nullptr);
+    if (!window)
+    {
+        glfwTerminate();
+        window = nullptr;
+        fprintf(stderr, "Could not initialize GLFW window\n");
+        return -1;
+    }
+
+    glfwSetKeyCallback(window, key_callback);
+
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
+
+    glfwSetWindowShouldClose(window, GLFW_FALSE);
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    window = nullptr;
+
+    glfwTerminate();
+    return 0;
+}
